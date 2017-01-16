ebee0f89c135f1337ebd31c30d3c91c5136ccf79
Rochester.Ting <694526771@qq.com>
2017-01-16 23:45:35 +0800

    提交代码

---
+++ XMLY/XMLY/Classes/Main(主要)/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  XMLY
+//
+//  Created by Rochester on 16/1/17.
+//  Copyright © 2017年 Rochester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        navigationItem.title = "发现"
+    }
+}
+extension ViewController{
+    
+}
+
