5b6036331ef9f974b7bb87f4401134571f906f56
Rochester.Ting <694526771@qq.com>
2017-01-17 14:06:33 +0800

    22

--- XMLY/XMLY/Classes/Discover(发现)/Controller/DiscoverVC.swift
+++ XMLY/XMLY/Classes/Discover(发现)/Controller/DiscoverVC.swift
@@ -7,29 +7,26 @@
 //
 
 import UIKit
-
+let screenW : CGFloat = UIScreen.main.bounds.size.width
+let screenH : CGFloat = UIScreen.main.bounds.size.height
+let kStatusH : CGFloat = 20
+let kNavH : CGFloat = 44
 class DiscoverVC: UIViewController {
-
+    let titles = ["推荐","分类","广播","榜单","主播"]
+    lazy var titleView : UIView = {[weak self] in
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: kStatusH + kNavH, width: screenW, height: 49)
+        
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.red
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setUpUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+}
+extension DiscoverVC{
+    fileprivate func setUpUI(){
+        view.addSubview(titleView)
     }
-    */
-
 }
